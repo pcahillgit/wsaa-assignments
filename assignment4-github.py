8057298dbfc8e1b18e00d28d72c6f5ce2b1e72c2
Paul Cahill <paulcahill97@outlook.com>
2025-03-21 17:58:08 +0000

    assignment 4

--- assignment4-github.py
+++ assignment4-github.py
@@ -1,8 +1,32 @@
-# This program Reads a file from a repository, replaces all instances of "Andrew" with "Paul"
+# This program reads a file from a repository, replaces all instances of "Andrew" with "Paul"
 # and then commits those changes and pushes the file back to the repository. 
 # Author: Paul Cahill
 
+from github import Github
+from config import config as cfg
 
+apikey = cfg["githubkey"] # the config file with my github key is hidden by the gitignore.
+
+g = Github(apikey)
+repo = g.get_repo("pcahillgit/wsaa-assignments") # accessing the repo
+
+file_path = "sampletext.txt" # text to be edited
+fileInfo = repo.get_contents(file_path)
+file_content = fileInfo.decoded_content.decode("utf-8")  # Reading the file contents
+updated_content = file_content.replace("Andrew", "Paul") # Replacing Andrew with Paul in the text
+
+# Committing if there are changes
+if file_content != updated_content:
+    repo.update_file(
+        path=file_path,
+        message="Ran assignment 4, updated sampletext.txt and replaced 'Andrew' with 'Paul'",
+        content=updated_content,
+        sha=fileInfo.sha,
+    )
+    print("Done!")
+else:
+    print("No changes needed.")
 
 # References:
-#
+# Andrew Beatty: Web Services and Applications Lab 5.03 Using Packages.
+# Github Guide for Personal Access Tokens: https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens
